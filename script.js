337dcf9a3d633eb0b36a77951e2d2eca4856cfee
Jordan-182 <jordan.p62@outlook.fr>
2024-10-13 14:52:09 +0200

    ajout classe active nav

--- script.js
+++ script.js
@@ -22,6 +22,9 @@ new ResizeObserver(entries =>{
 
 // FIN DU RESPONSIVE DE LA NAV //
 
+
+// Cette partie gère le scroll au clic des liens de la nav
+
 document.querySelectorAll('.navlinks-container a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
@@ -41,7 +44,40 @@ document.querySelectorAll('.navlinks-container a').forEach(anchor => {
     });
 });
 
-const scrollToTop = document.querySelector(".text-logo a");
+// Cette partie gère la classe active de la nav qui met l'indicateur sous le lien correspondant à la section en lecture
+
+const sections = document.querySelectorAll('section'); // Récupère les sections correspondant aux liens de la nav
+const links = document.querySelector('.navlinks-container a'); // Récupère les liens de la nav
+
+const observerOptions = {
+    root: null, // Utilise la fenêtre du navigateur comme root
+    rootMargin: '0px',
+    threshold: 0.5 // A changer selon les besoins (0.5 signifie que 50% de la section doit être visible)
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        const id = entry.target.getAttribute('id'); // Prends l'ID de la section
+        const navLink = document.querySelector(`.navlinks-container a[href="#${id}"]`);
+
+        if (entry.isIntersecting) {
+            // Ajouter la classe active au lien correspondant
+            links.querySelectorAll('a').forEach(link => link.classList.remove('active'));
+            navLink.classList.add('active');
+        } else{
+            navLink.classList.remove('active');
+        };
+    });
+}, observerOptions);
+
+// Observer chaque section
+sections.forEach(section => {
+    observer.observe(section);
+});
+
+// Cette partie gère le scroll to top en cliquant sur le logo
+
+const scrollToTop = document.querySelector(".nav-icon");
 scrollToTop.addEventListener("click", (e) => {
     e.preventDefault();
     window.scrollTo({
